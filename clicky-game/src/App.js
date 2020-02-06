23eb2093296d64f51489087c86b292b207916a44
Casey Moldavon <cmoldavon@gmail.com>
2020-02-06 10:36:27 -0800

    Structuring files and folders

--- clicky-game/src/App.js
+++ clicky-game/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import icons from "./icons.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Setting this.state.icons to the icons json array
+  state = {
+    icons
+  };
+
+  iconSelected = id => {
+    // Filter this.state.icons for icons with an id not equal to the id being removed
+    const icons = this.state.icons.filter(icon => icon.id !== id);
+    // Set this.state.icons equal to the new icons array
+    this.setState({ icons });
+  };
+
+  // Map over this.state.icons and render a FriendCard component for each icon object
+  render() {
+    return (
+      <Wrapper>
+        <Title>Friends List</Title>
+        {this.state.icons.map(icon => (
+          <FriendCard
+            iconSelected={this.iconSelected}
+            id={icon.id}
+            key={icon.id}
+            name={icon.name}
+            image={icon.image}
+          />
+        ))}
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
