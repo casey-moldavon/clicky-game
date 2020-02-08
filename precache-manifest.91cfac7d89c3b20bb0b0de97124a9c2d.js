80aea076a91c156f753fced0ba9494ff4b14c00c
Casey Moldavon <cmoldavon@gmail.com>
2020-02-07 22:05:00 -0800

    Updates

---
+++ precache-manifest.91cfac7d89c3b20bb0b0de97124a9c2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8a806ca6230e790df6eefde64eeb108",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "bf03f1efedf3e5f788c7",
+    "url": "/clicky-game/static/css/main.5e5fa6dc.chunk.css"
+  },
+  {
+    "revision": "95f075080f07f5af97ce",
+    "url": "/clicky-game/static/js/2.9d87d70b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/clicky-game/static/js/2.9d87d70b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf03f1efedf3e5f788c7",
+    "url": "/clicky-game/static/js/main.31c7c890.chunk.js"
+  },
+  {
+    "revision": "b80b4d3d407fbcfa1eb9",
+    "url": "/clicky-game/static/js/runtime-main.eb9d1727.js"
+  },
+  {
+    "revision": "9e424fee2672cb3f71301f14cec6ac3a",
+    "url": "/clicky-game/static/media/smbfont 2.9e424fee.ttf"
+  }
+]);
